de31cf4a001dc5a8bcff9b8bbbe4ecb4a3595261
Yu Hong Yiu <97735756+217013277@users.noreply.github.com>
2022-09-06 18:40:18 +0800

    implement recyclerview scrollview

--- app/src/main/java/com/example/bigbigshopre_design/Category.kt
+++ app/src/main/java/com/example/bigbigshopre_design/Category.kt
@@ -1,7 +1,10 @@
 package com.example.bigbigshopre_design
 
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -84,6 +87,7 @@ class Category : Fragment(), BookClickListener {
 
     override fun onClick(book: Book) {
         val intent = Intent(activity?.applicationContext, DetailActivity::class.java)
+        Log.i(TAG,book.id.toString())
         intent.putExtra(BOOK_ID_EXTRA, book.id)
         startActivity(intent)
     }
