aa4547b53e0e195d943dcf80e7c7b1850e63cf0b
Yu Hong Yiu <97735756+217013277@users.noreply.github.com>
2022-09-06 12:01:42 +0800

    create basic navgation bar and fragment

--- app/src/main/java/com/example/bigbigshopre_design/MainActivity.kt
+++ app/src/main/java/com/example/bigbigshopre_design/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.bigbigshopre_design
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.bigbigshopre_design.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(Home())
+                R.id.favourite -> replaceFragment(Favourite())
+                R.id.category -> replaceFragment(Category())
+                R.id.cart -> replaceFragment(Cart())
+                R.id.profile -> replaceFragment(Profile())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
     }
 }
