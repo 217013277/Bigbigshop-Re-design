fbf9892004da44f1a0be6c40786927c64679f8f1
Yu Hong Yiu <97735756+217013277@users.noreply.github.com>
2022-09-10 15:43:55 +0800

    create back to last activity

--- app/src/main/java/com/example/bigbigshopre_design/CheckoutActivity.kt
+++ app/src/main/java/com/example/bigbigshopre_design/CheckoutActivity.kt
@@ -2,10 +2,16 @@ package com.example.bigbigshopre_design
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bigbigshopre_design.databinding.ActivityCheckoutBinding
+import com.example.bigbigshopre_design.databinding.ActivityMainBinding
 
 class CheckoutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCheckoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_checkout)
+        binding = ActivityCheckoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.breadcrumb1.setOnClickListener { finish() }
     }
 }
