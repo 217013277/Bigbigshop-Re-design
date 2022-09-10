a5540fbf45989ebf7f8b3b7499a46e86965f4dc5
Yu Hong Yiu <97735756+217013277@users.noreply.github.com>
2022-09-10 16:30:21 +0800

    create CheckoutActivity.kt form

--- app/src/main/java/com/example/bigbigshopre_design/CheckoutActivity.kt
+++ app/src/main/java/com/example/bigbigshopre_design/CheckoutActivity.kt
@@ -2,6 +2,8 @@ package com.example.bigbigshopre_design
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.bigbigshopre_design.databinding.ActivityCheckoutBinding
 import com.example.bigbigshopre_design.databinding.ActivityMainBinding
 
@@ -13,5 +15,29 @@ class CheckoutActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.breadcrumb1.setOnClickListener { finish() }
+
+        binding.sameInformationButton.setOnClickListener {
+            if(!binding.sameInformationButton.isChecked) {
+                binding.sameInformation.visibility = View.VISIBLE
+            } else {
+                binding.sameInformation.visibility = View.GONE
+            }
+        }
+
+        binding.nextButton.setOnClickListener {
+            checkValidation()
+        }
+
+
+    }
+
+    private fun checkValidation() {
+        if (binding.checkoutMethodRadioGroup.checkedRadioButtonId == -1) {
+            // no radio buttons are checked
+            Toast.makeText(this, "Payment method not checked", Toast.LENGTH_SHORT).show()
+        } else {
+            // one of the radio buttons is checked
+            Toast.makeText(this, "Payment method checked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
